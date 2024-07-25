969374e5545815f112f3c80809d6d64f761bd7d8
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2024-07-25 14:49:47 +0700

    remove

--- lib/screens/startScreen.dart
+++ lib/screens/startScreen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/models/pages.dart';
-import 'package:my_app/screens/detailscreen.dart';
 import 'package:my_app/screens/onboardscreen.dart';
-import 'package:my_app/widgets/indicator.dart';
 // import 'package:my_app/screens/profilescreen.dart';
 // import 'package:my_app/screens/detailScreen.dart';
 // import 'package:my_app/screens/mainscreen.dart';
